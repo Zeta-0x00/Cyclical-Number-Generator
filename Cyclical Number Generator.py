16c719d4e48a6eae55e23e22603490a4b93b964d
Zeta <38230184+Zeta7@users.noreply.github.com>
2019-10-09 13:49:41 -0600

    Optimization
    Optimization algorithm

--- Cyclical Number Generator.py
+++ Cyclical Number Generator.py
@@ -11,21 +11,23 @@ def IsPrime(number):
             flag = False
             break
     return flag
-def Cicles(p):
+def Cicles(p,bFlag):
     if(IsPrime(p)):
         l = []
         cont = 2
         while (cont<p):
             j = []
-            for i in range(1, p):
-                j.append(pow(cont%p,i%p)%p)
             flag = True
-            for i in j:
-                if(j.count(i) != 1):
+            for i in range(1, p):
+                if(pow(cont%p,i%p)%p in j):
                     flag = False
+                else:
+                    j.append(pow(cont%p,i%p)%p)
             if(flag):
+                if(bFlag):
+                    print(cont)
                 l.append(cont)
-            cont +=1
+            cont += 1
         return l
 def ErrorCatch():
     n = input("Try again?[Y|N]\n>>> ")
@@ -36,13 +38,13 @@ def ErrorCatch():
     else:
         os.system(clear)
         return ErrorCatch()
-def Main():
+def Main():    
     while True:
         os.system(clear)
         try:
-            print("\t\tCyclical Number Generator v0.1\n\t\t\tpor Zeta Hoffman")
+            print("\t\tCyclical Number Generator v0.2\n\t\t\tby Zeta Hoffman")
             n = int(input("Enter a Prime Number\n>>> "))
-            cicles = Cicles(n)
+            cicles = Cicles(n,True)
             print("The generators os cycles of {} are {}".format(n,cicles))
             input("Press \'Enter\' to continue . . . ")
         except:
